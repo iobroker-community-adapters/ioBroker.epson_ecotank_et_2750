d19624605e0c6f1137d1d1491aa2c2bb64975efc
Dennis Hinzpeter <o0shojo0o@dietru.de>
2021-01-14 18:25:13 +0100

    init

---
+++ README.md
@@ -0,0 +1,65 @@
+![Logo](admin/epson_ecotank_et_2750.png)
+# ioBroker.epson_ecotank_et_2750
+
+![Number of Installations](https://iobroker.live/badges/epson_ecotank_et_2750-installed.svg) ![Number of Installations](https://iobroker.live/badges/epson_ecotank_et_2750-stable.svg) [![NPM version](https://img.shields.io/npm/v/iobroker.epson_ecotank_et_2750.svg)](https://www.npmjs.com/package/iobroker.epson_ecotank_et_2750)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.epson_ecotank_et_2750.svg)](https://www.npmjs.com/package/iobroker.epson_ecotank_et_2750)
+
+[![NPM](https://nodei.co/npm/iobroker.epson_ecotank_et_2750.png?downloads=true)](https://nodei.co/npm/iobroker.epson_ecotank_et_2750/)
+
+## Seq adapter for ioBroker
+
+This adapter read tank level and other information from EPSON EcoTank ET-2750 and store in ioBroker.
+
+## Configuration
+
+1. Create a new instance of the adapter
+2. Fill the URL/IP and port of the EPSON EcoTank ET-2750
+3. Configur ther Synctime (default 180 minutes)
+4. Save the settings
+
+## Changelog
+<!--
+ https://github.com/AlCalzone/release-script#usage
+    npm run release minor -- --all 0.9.8 -> 0.10.0
+    npm run release patch -- --all 0.9.8 -> 0.9.9
+    npm run release prerelease beta -- --all v0.2.1 -> v0.2.2-beta.0  
+	Placeholder for the next version (at the beginning of the line):
+	### __WORK IN PROGRESS__
+-->
+### __WORK IN PROGRESS__
+* (o0shojo0o) Add compact mode
+* (o0shojo0o) All necessary changes for EPSON EcoTank ET-2750
+* (o0shojo0o) New tree structure
+* (o0shojo0o) Replacing the request with axios npm module
+
+### 0.0.1 (2021-01-03)
+* (o0shojo0o) forked from iobroker.epson_stylus_px830 0.2.1
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2021 o0shojo0o
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
+*Dank an die Erfinder des Basisskripts zum Parsen der Daten, Idittmar und MistyReblaus aus dem [Homematic-Forum](http://homematic-forum.de/forum/viewtopic.php?f=31&t=25140).* :+1: 
+
+*Dank an pix und rr0v1 für die Vorlage
---
+++ main.js
@@ -0,0 +1,285 @@
+/* jshint -W097 */ // jshint strict:false
+/*jslint node: true */
+
+"use strict";
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
+const axios = require('axios').default;
+const lang = 'de';
+let callReadPrinter;
+let ip = '';
+const baselevel = 50; // bedeutet: in der Webseite wird ein Balken von 100% Höhe 50px hoch gezeichnet. 
+                    // Also entspricht ein gezeigtes Tintenlevel von 25 (px) dann 50% und eines von 10 (px) dann 20%
+let sync = 180;
+let adapter;
+
+function startAdapter(options) {
+    options = options || {};
+    Object.assign(options, {
+        name: 'epson_ecotank_et_2750',
+        systemConfig: true,
+        useFormatDate: true,
+        unload: function (callback) {
+            try {
+                stopReadPrinter();
+                callback();
+            } catch (e) {
+                callback();
+            }
+        },
+        ready: function () {
+            readSettings();
+            main();         
+            adapter.setState('info.connection', false, true);   
+        }
+    });
+    adapter = new utils.Adapter(options);
+    return adapter;
+};
+
+const ink = {
+    'cyan' : {
+        'state': 'cyan',
+        'name': 'Cyan',
+        'inklvl_rx':  "IMAGE\\/Ink_C\\.PNG\\' height=\\'([0-9]{1,2})\\'",
+        'cartridge_rx': "\\(C\\)&nbsp;\\:<\\/span><\\/dt><dd class=\\\"value clearfix\\\"><div class=\\\"preserve-white-space\\\">([a-zA-Z0-9\\/]*)<\\/div>"
+    },
+    'yellow' : {
+        'state': 'yellow',
+        'name': 'Yellow',
+        'inklvl_rx':  "IMAGE\\/Ink_Y\\.PNG\\' height=\\'([0-9]{1,2})\\'",
+        'cartridge_rx': "\\(Y\\)&nbsp;\\:<\\/span><\\/dt><dd class=\\\"value clearfix\\\"><div class=\\\"preserve-white-space\\\">([a-zA-Z0-9\\/]*)<\\/div>"    
+    },
+    'black' : {
+        'state': 'black',
+        'name': 'Black',
+        'inklvl_rx':  "IMAGE\\/Ink_K\\.PNG\\' height=\\'([0-9]{1,2})\\'",
+        'cartridge_rx': "\\(BK\\)&nbsp;\\:<\\/span><\\/dt><dd class=\\\"value clearfix\\\"><div class=\\\"preserve-white-space\\\">([a-zA-Z0-9\\/]*)<\\/div>" 
+    },
+    'magenta' : {
+        'state': 'magenta',
+        'name': 'Magenta',
+        'inklvl_rx':  "IMAGE\\/Ink_M\\.PNG\\' height=\\'([0-9]{1,2})\\'",
+        'cartridge_rx': "\\(M\\)&nbsp;\\:<\\/span><\\/dt><dd class=\\\"value clearfix\\\"><div class=\\\"preserve-white-space\\\">([a-zA-Z0-9\\/]*)<\\/div>"    
+    }
+};
+
+function readSettings() {
+    //check if IP is entered in settings
+    
+    if (!adapter.config.printerip) {
+        adapter.log.warn('No IP adress of printer set up. Adapter will be stopped.');
+    } 
+    else { // ip entered
+        ip = (adapter.config.printerport.length > 0) ? adapter.config.printerip + ':' + adapter.config.printerport : adapter.config.printerip; // if port is set then ip+port else ip only
+        adapter.log.debug('IP: ' + ip);
+    
+        //check if sync time is entered in settings
+        sync = (!adapter.config.synctime) ? 180 : parseInt(adapter.config.synctime,10);
+        adapter.log.debug('ioBroker reads printer every ' + sync + ' minutes');
+
+    } // end ip entered
+}
+
+async function readPrinterStatus() {
+
+    const link = 'http://' + ip + '/PRESENTATION/ADVANCED/INFO_PRTINFO/TOP';
+ 
+    adapter.setState('info.ip', {
+        val: ip,
+        ack: false
+    });
+ 
+    const resp = await axios.get(link);
+    if (resp.status === 200) {    
+        
+        adapter.setState('info.ip', {
+            val: ip,
+            ack: true
+        });
+        
+        let match, rx;
+        // MAC ADRESSE EINLESEN
+        rx = new RegExp( /(?:MAC-Adresse|Printer Name|Adresse MAC Wi-Fi\/R.seau|Indirizzo MAC Wi-Fi\/rete|Dirección MAC de Wi-Fi\/Red|Endereço MAC de Wi-Fi\/Rede)&nbsp;:<\/span><\/dt><dd class=\"value clearfix\"><div class=\"preserve-white-space\">([a-zA-Z0-9:]*)<\/div>/g );
+        let mac_string;
+        while((match = rx.exec(resp.data)) != null) {
+                mac_string = match[1];
+        }
+        adapter.log.debug('mac_string: ' + mac_string);
+        adapter.setState('info.mac', {val: mac_string, ack: true});
+        
+        // read firmware version
+        rx = new RegExp( /(?:Firmware)&nbsp;:<\/span><\/dt><dd class=\"value clearfix\"><div class=\"preserve-white-space\">([a-zA-Z0-9 äöüÄÖÜ\-\_\.]*)<\/div>/g );
+        let firmware_string;
+        while((match = rx.exec(resp.data)) != null) {
+            firmware_string = match[1];
+        }
+        adapter.log.debug('firmware_string: ' + firmware_string);
+        adapter.setState('info.firmware', {val: firmware_string, ack: true});
+
+        // read serial number
+        rx = new RegExp( /(?:Seriennummer|Serial Number|Numéro de série|Numero di serie|Número de serie|Número de série)&nbsp;:<\/span><\/dt><dd class=\"value clearfix\"><div class=\"preserve-white-space\">([a-zA-Z0-9]*)<\/div>/g );
+        let serial_string;
+        while((match = rx.exec(resp.data)) != null) {
+            serial_string = match[1];
+        }
+        adapter.log.debug('serial_string: ' + serial_string);
+        adapter.setState('info.serial', {val: serial_string, ack: true});
+
+        for (const i in ink) {
+            adapter.setObjectNotExists(`inks.${ink[i].state}`, {
+                type: 'state',
+                common: {
+                    role: 'level.volume',
+                    name: 'Level of ' + ink[i].name,
+                    desc: 'Level of ' + ink[i].name,
+                    type: 'number',
+                    unit: '%',
+                    read: true,
+                    write: false
+                },
+                native: {}
+            });               
+        
+            // read levels
+            rx = new RegExp(ink[i].inklvl_rx, "g");
+            let level_string;
+            while((match = rx.exec(resp.data)) != null) {
+                level_string = match[1];
+            }
+            adapter.log.debug(ink[i].name + ' Levelstring: ' + level_string + 'px');
+            const level = parseInt(level_string, 10) * 100 / baselevel;
+            adapter.setState(`inks.${ink[i].state}`, {val: level, ack: true});
+            adapter.log.debug(ink[i].name + ' Level: ' + level + '%');                    
+        } // end for
+        
+        adapter.log.debug('Channels and states created/read');
+        
+    } else {
+        adapter.log.warn('Cannot connect to Printer: ' + error);       
+    }  
+    adapter.log.debug('finished reading printer status data');
+}
+
+ async function readPrinterNetwork() {
+
+    const link = 'http://' + ip + '/PRESENTATION/ADVANCED/INFO_NWINFO/TOP';
+ 
+    adapter.setState('info.ip', {
+        val: ip,
+        ack: false
+    });
+
+    const resp = await axios.get(link);
+    if (resp.status === 200) {           
+      
+        adapter.setState('info.ip', {
+            val: ip,
+            ack: true
+        });
+        
+        var match, rx;
+        // NAME EINLESEN
+        rx = new RegExp( /(?:Gerätename)&nbsp;:<\/span><\/dt><dd class=\"value clearfix\"><div class=\"preserve-white-space\">([a-zA-Z0-9 äöüÄÖÜ\-\_]*)<\/div>/g );
+        while((match = rx.exec(resp.data)) != null) {
+            var name_string = match[1];
+        }
+        adapter.log.debug('name_string: ' + name_string);
+        adapter.setState('info.name', {val: name_string, ack: true});
+        
+        // MODELL EINLESEN
+        rx = new RegExp( /<title>([a-zA-Z0-9 äöüÄÖÜ\-\_]*)<\/title>/g );
+        while((match = rx.exec(resp.data)) != null) {
+            var model_string = match[1];
+        }
+        adapter.log.debug('model_string: ' + model_string);
+        adapter.setState('info.model', {val: model_string, ack: true});
+        
+        adapter.log.debug('Channels and states created/read');
+        
+    } else {
+        adapter.log.warn('Cannot connect to Printer: ' + error);
+    }  
+    adapter.log.debug('finished reading printer network data');
+}
+
+async function readPrinterMaintenance() {
+
+    const link = 'http://' + ip + '/PRESENTATION/ADVANCED/INFO_MENTINFO/TOP';
+ 
+    adapter.setState('info.ip', {
+        val: ip,
+        ack: false
+    });
+    
+    const resp = await axios(link);
+    if (resp.status === 200) {
+
+        adapter.setState('info.ip', {
+            val: ip,
+            ack: true
+        });
+        
+        var match, rx;
+        // ERSTDRUCKDATUM EINLESEN
+        rx = new RegExp( /(?:Erstdruckdatum|First Printing Date|Date de première impression|Data prima stampa|Primera fecha de impresión|Data da primeira impressão)&nbsp;\:<\/span><\/dt><dd class=\"value clearfix\"><div class=\"preserve-white-space\">(\d\d\d\d\-\d\d\-\d\d)<\/div>/g );
+        while((match = rx.exec(resp.data)) != null) {
+            var first_print_string = match[1];
+        }
+        adapter.log.debug('first_print_string: ' + first_print_string);
+        adapter.setState('info.first_print_date', {val: first_print_string, ack: true});
+        
+        // GESAMTZAHL SEITEN
+        rx = new RegExp( /(?:Gesamtanzahl Seiten)&nbsp;\:<\/span><\/dt><dd class=\"value clearfix\"><div class=\"preserve-white-space\">(\d*)<\/div>/g );
+        while((match = rx.exec(resp.data)) != null) {
+            var printed_pages_string = match[1];
+        }
+        adapter.log.debug('printed_pages_string: ' + printed_pages_string);
+        var page_count = parseInt(printed_pages_string, 10);
+        adapter.log.debug('page_count: ' + page_count);
+        adapter.setState('info.page_count', {val: page_count, ack: true});
+
+
+        adapter.log.debug('Channels and states created/read');
+        
+    } else {
+        adapter.log.warn('Cannot connect to Printer: ' + error);
+      
+    }   
+    adapter.log.debug('finished reading printer maintenance data');
+}
+
+function stopReadPrinter() {
+    clearTimeout(callReadPrinter);
+    adapter.setState('info.connection', false, true);
+    adapter.log.info('Epson EcoTank ET-2750 adapter stopped');
+}
+
+async function main() {   
+    try {      
+        adapter.log.debug('Request printer stats...');  
+        await readPrinterNetwork();
+        await readPrinterStatus();
+        await readPrinterMaintenance();
+        adapter.setState('info.connection', true, true);               
+    } catch (err) {
+        if (err.message.includes('EHOSTUNREACH')) {                
+            adapter.log.debug(`Printer offline, next try in ${sync} minutes...`);  
+        } 
+        else {
+          adapter.log.error(JSON.stringify(err));
+        }
+        adapter.setState('info.connection', false, true);
+    }
+    callReadPrinter = setTimeout(main, sync * 1000 * 60); 
+}
+
+
+
+// If started as allInOne/compact mode => return function to create instance
+if (module && module.parent) {
+    module.exports = startAdapter;   
+} else {
+    // or start the instance directly
+    startAdapter();   
+
+} 
